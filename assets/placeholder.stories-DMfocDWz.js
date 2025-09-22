f8138ff8bc93667af5e4d20ebef6fcd2648eadfb
fabio7maia <fabio7maia@users.noreply.github.com>
2025-09-22 17:37:17 +0000

    Deploy to GitHub pages

---
+++ assets/placeholder.stories-DMfocDWz.js
@@ -0,0 +1,9 @@
+import{R as o}from"./index-D4lIrffr.js";import{S as i,P as d}from"./placeholder.component-5IK4axi2.js";import"./logger.helper-B8kJPxwa.js";const h=i.writeStory({component:d,group:"Components"}),P={...h.meta,title:"Components/Placeholder"},p=m=>o.createElement("div",{style:{height:"50px"}},o.createElement(d,{...m})),e=p.bind({});e.args={children:o.createElement("h1",null,"Placeholder example"),loading:!0};const r=p.bind({});r.args={children:o.createElement("h1",null,"Placeholder example"),loading:!1};var a,t,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`props => <div style={{
+  height: '50px'
+}}>
+        <Placeholder {...props} />
+    </div>`,...(s=(t=e.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};var n,l,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`props => <div style={{
+  height: '50px'
+}}>
+        <Placeholder {...props} />
+    </div>`,...(c=(l=r.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};const y=["Loading","Loaded"];export{r as Loaded,e as Loading,y as __namedExportsOrder,P as default};
